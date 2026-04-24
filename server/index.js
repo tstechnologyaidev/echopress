60cb477195063fd982273a1a261f134075ca7853
Théo Forest Tran <foresttrant@educ.college-montreal.qc.ca>
2026-04-24 17:34:20 -0400

    Fixed admin panel quill

--- server/index.js
+++ server/index.js
@@ -32,22 +32,22 @@ console.log("Process started. Initializing server...");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "img-src": ["'self'", "data:", "blob:", "https://*.supabase.co", "https://*.youtube.com", "https://*.ytimg.com"],
-      "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://*.youtube.com", "https://www.youtube.com", "https://s.ytimg.com", "https://cdn.quilljs.com"],
-      "frame-src": ["'self'", "https://*.youtube.com", "https://www.youtube.com", "https://www.youtube-nocookie.com"],
-      "connect-src": ["'self'", "https://*.supabase.co", "https://vitals.vercel-insights.com"],
-      "font-src": ["'self'", "https://fonts.gstatic.com", "data:"],
-      "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdn.quilljs.com"]
-    },
-  },
-  crossOriginEmbedderPolicy: false,
-  crossOriginResourcePolicy: { policy: "cross-origin" },
-  referrerPolicy: { policy: "strict-origin-when-cross-origin" }
-}));
+// app.use(helmet({
+//   contentSecurityPolicy: {
+//     directives: {
+//       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//       "img-src": ["'self'", "data:", "blob:", "https://*.supabase.co", "https://*.youtube.com", "https://*.ytimg.com"],
+//       "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://*.youtube.com", "https://www.youtube.com", "https://s.ytimg.com", "https://cdn.quilljs.com"],
+//       "frame-src": ["'self'", "https://*.youtube.com", "https://www.youtube.com", "https://www.youtube-nocookie.com"],
+//       "connect-src": ["'self'", "https://*.supabase.co", "https://vitals.vercel-insights.com"],
+//       "font-src": ["'self'", "https://fonts.gstatic.com", "data:"],
+//       "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdn.quilljs.com"]
+//     },
+//   },
+//   crossOriginEmbedderPolicy: false,
+//   crossOriginResourcePolicy: { policy: "cross-origin" },
+//   referrerPolicy: { policy: "strict-origin-when-cross-origin" }
+// }));
 app.use(cors());
 app.use(express.json({ limit: '1000gb' }));
 app.use(express.urlencoded({ extended: true, limit: '1000gb' }));
